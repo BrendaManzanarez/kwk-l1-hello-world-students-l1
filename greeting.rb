8935e99f2353d9c7ab18df055b5340f42e7cf598
Brenda Manzanarez <brendamanzanarez.link@gmail.com>
2018-06-04 23:02:21 +0000

    automatically backed up by learn

--- greeting.rb
+++ greeting.rb
@@ -2,6 +2,6 @@ def greeting
   your_name = "Link"
 my_name = "Kookie" 
 local_greeting = "Hi"
-puts "#{local_greeting} your_name! my name is my_name. how was your day?"
+puts "#{local_greeting} #{your_name}! my name is #{my_name}. how was your day?"
 end
 greeting
